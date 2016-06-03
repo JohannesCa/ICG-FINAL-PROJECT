877d827c280e05f7f3e6c5d94b158008b86f0a2c
Thiago Bezerra <bezerrathm@gmail.com>
2016-06-03 12:29:26 -0300

    Textures

--- src/main.cpp
+++ src/main.cpp
@@ -48,7 +48,7 @@ int main()
 
 
     // Build and compile our shader program
-    Shader ourShader("shaders/vertexshader.vs", "shaders/fragmentshader.frag");
+    Shader ourShader("shaders/texture.vs", "shaders/texture.frag");
 
 
     // Set up vertex data (and buffer(s)) and attribute pointers
@@ -105,7 +105,7 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // Load, create texture and generate mipmaps
     int width, height;
-    unsigned char* image = SOIL_load_image("textures/wall.jpg", &width, &height, 0, SOIL_LOAD_RGB);
+    unsigned char* image = SOIL_load_image("textures/brickwall.jpg", &width, &height, 0, SOIL_LOAD_RGB);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
     glGenerateMipmap(GL_TEXTURE_2D);
     SOIL_free_image_data(image);
@@ -122,7 +122,7 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // Load, create texture and generate mipmaps
-    image = SOIL_load_image("textures/wall.jpg", &width, &height, 0, SOIL_LOAD_RGB);
+    image = SOIL_load_image("textures/christian.jpg", &width, &height, 0, SOIL_LOAD_RGB);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
     glGenerateMipmap(GL_TEXTURE_2D);
     SOIL_free_image_data(image);
