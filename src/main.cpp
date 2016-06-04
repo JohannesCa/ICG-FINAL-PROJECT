26a8e1ee567fa624551cfd8dd66b687e0fb6108e
Thiago Bezerra <bezerrathm@gmail.com>
2016-06-04 20:34:28 -0300

    Parallel

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,9 @@
 // Other Libs
 #include <SOIL.h>
 
+#include <thread>
+#include <unistd.h>
+
 // Properties
 GLuint screenWidth = 800, screenHeight = 600;
 
@@ -39,99 +42,118 @@ bool firstMouse = true;
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
 
-#include <thread>
 
 // The MAIN function, from here we start our application and run our Game loop
 int main()
 {
-    // Init GLFW
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "LearnOpenGL", nullptr, nullptr); // Windowed
+//Inicialização da janela - - - -
 
-    glfwMakeContextCurrent(window);
+	// Init GLFW
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    // Set the required callback functions
-    glfwSetKeyCallback(window, key_callback);
-    glfwSetCursorPosCallback(window, mouse_callback);
-    glfwSetScrollCallback(window, scroll_callback);
+	GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "LearnOpenGL", nullptr, nullptr); // Windowed
 
-    // Options
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-    // Initialize GLEW to setup the OpenGL Function pointers
-    glewExperimental = GL_TRUE;
-    glewInit();
-
-    // Define the viewport dimensions
-    glViewport(0, 0, screenWidth, screenHeight);
-
-    // Setup some OpenGL options
-    glEnable(GL_DEPTH_TEST);
-
-    // Setup and compile our shaders
-    Shader shader("resources/shaders/model_loading.vs", "resources/shaders/model_loading.frag");
-
-
-    bool LOADED = false;
-
-    // Load models
-    Model* ourModel;
-
-    auto t = std::thread([&]{
 	glfwMakeContextCurrent(window);
-	ourModel = new Model("resources/models/FinalBaseMesh.obj");
-	LOADED = true;
+
+	// Set the required callback functions
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	// Options
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// Initialize GLEW to setup the OpenGL Function pointers
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	// Define the viewport dimensions
+	glViewport(0, 0, screenWidth, screenHeight);
+
+	// Setup some OpenGL options
+	glEnable(GL_DEPTH_TEST);
+
+//Shaders - - - -
+
+	// Setup and compile our shaders
+	Shader shader("resources/shaders/model_loading.vs", "resources/shaders/model_loading.frag");
+
+//Model - - - -
+
+
+	Model* mapModel;
+	bool LOADED = false;
+
+	std::thread* t = new std::thread([&]{
+		glfwMakeContextCurrent(window);//Atribui o contexto
+		mapModel = new Model("resources/models/FinalBaseMesh.obj"); //carrega o objeto
+		std::cerr << "LOADED" << std::endl;
+		LOADED = true; //avisa que já pode ser renderizado
 	});
+
 
     // Draw in wireframe
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    // Game loop
-    while(!glfwWindowShouldClose(window))
-    {
-        // Set frame time
-        GLfloat currentFrame = glfwGetTime();
-        deltaTime = currentFrame - lastFrame;
-        lastFrame = currentFrame;
 
-        // Check and call events
-        glfwPollEvents();
-        Do_Movement();
+//LOOP - - - -
+	// Game loop
+	while(!glfwWindowShouldClose(window))
+	{
 
-        // Clear the colorbuffer
-        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        	// Set frame time
+	        GLfloat currentFrame = glfwGetTime();
+	        deltaTime = currentFrame - lastFrame;
+	        lastFrame = currentFrame;
 
-	if(LOADED){
+	        // Check and call events
+	        glfwPollEvents();
+	        Do_Movement();
 
-        	shader.Use();   // <-- Don't forget this one!
-	        // Transformation matrices
-	        glm::mat4 projection = glm::perspective(camera.Zoom, (float)screenWidth/(float)screenHeight, 0.1f, 100.0f);
-	        glm::mat4 view = camera.GetViewMatrix();
-	        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-	        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
-	        // Draw the loaded model
-	        glm::mat4 model;
-	        model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // Translate it down a bit so it's at the center of the scene
-	        model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));	// It's a bit too big for our scene, so scale it down
-	        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-	        ourModel->Draw(shader);
+		//APÓS O CARREGAMENTO
+		if(LOADED){
+
+			glfwSetWindowTitle(window, "GAME");
+		        // Clear the colorbuffer
+		        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+	        	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        		shader.Use();   // <-- Don't forget this one!
+		        // Transformation matrices
+		        glm::mat4 projection = glm::perspective(camera.Zoom, (float)screenWidth/(float)screenHeight, 0.1f, 100.0f);
+		        glm::mat4 view = camera.GetViewMatrix();
+		        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+		        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+
+		        // Draw the loaded model
+		        glm::mat4 model;
+		        model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // Translate it down a bit so it's at the center of the scene
+		        model = glm::scale(model, glm::vec3(0.01f, 0.01f, 0.01f));	// It's a bit too big for our scene, so scale it down
+		        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+		        mapModel->Draw(shader);
+		}
+		else{
+			glfwSetWindowTitle(window, "MENU");
+		        // Clear the colorbuffer
+		        glClearColor(0.2f, 0.3f, 0.4f, 1.0f);
+		        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		}
+
+        	// Swap the buffers
+		glfwSwapBuffers(window);
 	}
 
-        // Swap the buffers
-        glfwSwapBuffers(window);
-    }
+	t->join();
 
-	t.join();
-
-    glfwTerminate();
-    return 0;
+	glfwTerminate();
+	return 0;
 }
 
 #pragma region "User input"
