2acec3a3d7dd32d71a113facad7b9107a23c5870
JohannesCa <johannes.cavalcante@outlook.com>
2016-06-04 21:54:33 -0300

    Colision with map and interaction.

--- include/Camera.h
+++ include/Camera.h
@@ -7,7 +7,7 @@
 #include <GL/glew.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-
+#include <glm/gtx/rotate_vector.hpp>
 
 
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
@@ -19,10 +19,10 @@ enum Camera_Movement {
 };
 
 // Default camera values
-const GLfloat YAW        = -90.0f;
+const GLfloat YAW        =  180.0f;
 const GLfloat PITCH      =  0.0f;
 const GLfloat SPEED      =  3.0f;
-const GLfloat SENSITIVTY =  0.25f;
+const GLfloat SENSITIVTY =  0.2f;
 const GLfloat ZOOM       =  45.0f;
 
 
@@ -70,17 +70,22 @@ public:
     }
 
     // Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
-    void ProcessKeyboard(Camera_Movement direction, GLfloat deltaTime)
+    void ProcessKeyboard(Camera_Movement direction, GLfloat deltaTime, GLfloat offsetY, GLboolean Colision = true)
     {
         GLfloat velocity = this->MovementSpeed * deltaTime;
         if (direction == FORWARD)
-            this->Position += this->Front * velocity;
+            this->Position += glm::normalize(glm::rotate(this->Front, glm::radians(this->Pitch), -this->Right)) * velocity;
         if (direction == BACKWARD)
-            this->Position -= this->Front * velocity;
+            this->Position -= glm::normalize(glm::rotate(this->Front, glm::radians(this->Pitch), -this->Right)) * velocity;
         if (direction == LEFT)
             this->Position -= this->Right * velocity;
         if (direction == RIGHT)
             this->Position += this->Right * velocity;
+
+        this->Position.y += offsetY;
+
+        if(Colision)
+            testMapColision();
     }
 
     // Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
@@ -130,4 +135,62 @@ private:
         this->Right = glm::normalize(glm::cross(this->Front, this->WorldUp));  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
         this->Up    = glm::normalize(glm::cross(this->Right, this->Front));
     }
+
+    void testMapColision()
+    {
+        // Test outer map boundaries
+        if(this->Position.z > 4.8)
+            this->Position.z = 4.8;
+        if(this->Position.z < -4.8)
+            this->Position.z = -4.8;
+        if(this->Position.x > 7.3)
+            this->Position.x = 7.3;
+        if(this->Position.x < -7.3)
+            this->Position.x = -7.3;
+
+
+        // Check intern map boundaries
+        if((this->Position.x > -2.2)&&(this->Position.x < 2.2)){
+            if((this->Position.z < 2.2)&&(this->Position.z > 2))
+                this->Position.z = 2.2;
+            if((this->Position.z > -2.2)&&(this->Position.z < -2))
+                this->Position.z = -2.2;
+            if((this->Position.z > 1.6)&&(this->Position.z < 2))
+                this->Position.z = 1.6;
+            if((this->Position.z < -1.6)&&(this->Position.z > -2))
+                this->Position.z = -1.6;
+        }
+
+        if((this->Position.x > -2.8)&&(this->Position.x < -1.9)){
+            if((this->Position.z < 1.2)&&(this->Position.z > 0.8))
+                this->Position.z = 1.2;
+            if((this->Position.z > -1.2)&&(this->Position.z < -0.8))
+                this->Position.z = -1.2;
+
+            if((this->Position.z < 1)&&(this->Position.z > -1)){
+                this->Position.y = 0.667 * (this->Position.x + 3) + 1.6; // Ramp equation
+                if((this->Position.z > 0.6)&&(this->Position.z < 1))
+                    this->Position.z = 0.6;
+                if((this->Position.z < -0.6)&&(this->Position.z > -1))
+                    this->Position.z = -0.6;
+            }
+        }
+
+        if((this->Position.z < 2.2)&&(this->Position.z > -2.2)){
+            if((this->Position.x > -2.3)&&(this->Position.x < -2)){
+                if((this->Position.z > 1)||(this->Position.z < -1))
+                    this->Position.x = -2.3;
+            }
+            if((this->Position.x < -1.6)&&(this->Position.x > -2)){
+                if((this->Position.z > 1)||(this->Position.z < -1))
+                    this->Position.x = -1.6;
+            }
+            if((this->Position.x < 2.3)&&(this->Position.x > 2))
+                this->Position.x = 2.3;
+            if((this->Position.x > 1)&&(this->Position.x < 2))
+                this->Position.x = 1;
+        }
+    }
 };
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -21,11 +21,11 @@
 // Other Libs
 #include <SOIL.h>
 
-#include <thread>
-#include <unistd.h>
-
 // Properties
-GLuint screenWidth = 800, screenHeight = 600;
+GLuint screenWidth = 1280, screenHeight = 720;
+GLfloat theta = 0;
+bool enableKey = true;
+bool enableExitKey = false;
 
 // Function prototypes
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
@@ -34,9 +34,9 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void Do_Movement();
 
 // Camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(4.0f, 1.6f, 0.0f));
 bool keys[1024];
-GLfloat lastX = 400, lastY = 300;
+GLfloat lastX, lastY;
 bool firstMouse = true;
 
 GLfloat deltaTime = 0.0f;
@@ -46,19 +46,17 @@ GLfloat lastFrame = 0.0f;
 // The MAIN function, from here we start our application and run our Game loop
 int main()
 {
-
-//Inicialização da janela - - - -
-
-	// Init GLFW
+// Initializing Window
+    // Init GLFW
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "LearnOpenGL", nullptr, nullptr); // Windowed
+    GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "Project Helios", nullptr, nullptr); // Windowed
 
-	glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(window);
 
 	// Set the required callback functions
 	glfwSetKeyCallback(window, key_callback);
@@ -78,19 +76,22 @@ int main()
 	// Setup some OpenGL options
 	glEnable(GL_DEPTH_TEST);
 
-//Shaders - - - -
+// Shaders - - - -
 
 	// Setup and compile our shaders
 	Shader shader("resources/shaders/model_loading.vs", "resources/shaders/model_loading.frag");
 
-//Model - - - -
+// Models - - - -
 
-
-	bool LOADED = false;
-
-	Model mapModel("resources/models/Mapa.obj");
-
-	LOADED = true;
+    // Load Models
+    Model Map("resources/models/Mapa.obj");
+    Model Bat1("resources/models/Huge Battery.obj");
+    Model Bat2("resources/models/Huge Battery.obj");
+    Model Bat3("resources/models/Huge Battery.obj");
+    Model Bat4("resources/models/Huge Battery.obj");
+    Model Keycard("resources/models/Keycard.obj");
+    Model KeyExit("resources/models/Keycard.obj");
+    Model Gun("resources/models/The Gun.obj");
 
     // Draw in wireframe
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -100,70 +101,137 @@ int main()
 	// Game loop
 	while(!glfwWindowShouldClose(window))
 	{
+        // Set frame time
+        GLfloat currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
 
-        	// Set frame time
-	        GLfloat currentFrame = glfwGetTime();
-	        deltaTime = currentFrame - lastFrame;
-	        lastFrame = currentFrame;
+        // Check and call events
+        glfwPollEvents();
+        Do_Movement();
 
-	        // Check and call events
-	        glfwPollEvents();
-	        Do_Movement();
+        // Clear the colorbuffer
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        shader.Use();   // <-- Don't forget this one!
 
 
-		//APÓS O CARREGAMENTO
-		if(LOADED){
+        // Transformation matrices
+        glm::mat4 projection = glm::perspective(camera.Zoom, (float)screenWidth/(float)screenHeight, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
-			glfwSetWindowTitle(window, "GAME");
-		        // Clear the colorbuffer
-		        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
-	        	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        		shader.Use();   // <-- Don't forget this one!
-		        // Transformation matrices
-		        glm::mat4 projection = glm::perspective(camera.Zoom, (float)screenWidth/(float)screenHeight, 0.1f, 100.0f);
-		        glm::mat4 view = camera.GetViewMatrix();
-		        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-		        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        // Draw the loaded model
+        glm::mat4 model_map;
+        glm::mat4 model_bat1;
+        glm::mat4 model_bat2;
+        glm::mat4 model_bat3;
+        glm::mat4 model_bat4;
+        glm::mat4 model_key;
+        glm::mat4 model_keyExit;
+        glm::mat4 model_gun;
 
-		        // Draw the loaded model
-		        glm::mat4 model;
-		        model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // Translate it down a bit so it's at the center of the scene
-		        model = glm::scale(model, glm::vec3(0.001f, 0.001f, 0.001f));	// It's a bit too big for our scene, so scale it down
-		        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-		        mapModel.Draw(shader);
-		}
-		else{
-			glfwSetWindowTitle(window, "MENU");
-		        // Clear the colorbuffer
-		        glClearColor(0.2f, 0.3f, 0.4f, 1.0f);
-		        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		}
+        // Prepare Matrices
+        model_map = glm::scale(model_map, glm::vec3(0.001f, 0.001f, 0.001f));
 
-        	// Swap the buffers
+        model_bat1 = glm::translate(model_bat1, glm::vec3(6.5f, 0.0f, 4.0f));
+        model_bat1 = glm::rotate(model_bat1, glm::radians(-135.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model_bat1 = glm::scale(model_bat1, glm::vec3(0.001f, 0.001f, 0.001f));
+
+        model_bat2 = glm::translate(model_bat2, glm::vec3(6.5f, 0.0f, -4.0f));
+        model_bat2 = glm::rotate(model_bat2, glm::radians(-45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model_bat2 = glm::scale(model_bat2, glm::vec3(0.001f, 0.001f, 0.001f));
+
+        model_bat3 = glm::translate(model_bat3, glm::vec3(6.5f, 0.0f, 0.0f));
+        model_bat3 = glm::rotate(model_bat3, glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model_bat3 = glm::scale(model_bat3, glm::vec3(0.001f, 0.001f, 0.001f));
+
+        model_bat4 = glm::translate(model_bat4, glm::vec3(-6.5f, 0.0f, 4.0f));
+        model_bat4 = glm::rotate(model_bat4, glm::radians(135.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model_bat4 = glm::scale(model_bat4, glm::vec3(0.001f, 0.001f, 0.001f));
+
+        model_key = glm::translate(model_key, glm::vec3(1.6f, 1.32f, -0.3f));
+        model_key = glm::rotate(model_key, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model_key = glm::scale(model_key, glm::vec3(0.001f, 0.001f, 0.001f));
+
+        model_keyExit = glm::translate(model_keyExit, glm::vec3(-6.25f, 1.2f, -4.99f));
+        model_keyExit = glm::rotate(model_keyExit, glm::radians(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model_keyExit = glm::scale(model_keyExit, glm::vec3(0.001f, 0.001f, 0.001f));
+
+
+        // Draw
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model_map));
+        Map.Draw(shader);
+
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model_bat1));
+        Bat1.Draw(shader);
+
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model_bat2));
+        Bat2.Draw(shader);
+
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model_bat3));
+        Bat3.Draw(shader);
+
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model_bat4));
+        Bat4.Draw(shader);
+
+
+        // Verify if the key was taken
+        if(enableKey){
+            glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model_key));
+            Keycard.Draw(shader);
+        }
+
+        if(enableExitKey){
+            glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model_keyExit));
+            KeyExit.Draw(shader);
+        }
+
+
+        // Swap the buffers
 		glfwSwapBuffers(window);
 	}
-
 
 	glfwTerminate();
 	return 0;
 }
+
 
 #pragma region "User input"
 
 // Moves/alters the camera positions based on user input
 void Do_Movement()
 {
-    // Camera controls
-    if(keys[GLFW_KEY_W])
-        camera.ProcessKeyboard(FORWARD, deltaTime);
-    if(keys[GLFW_KEY_S])
-        camera.ProcessKeyboard(BACKWARD, deltaTime);
-    if(keys[GLFW_KEY_A])
-        camera.ProcessKeyboard(LEFT, deltaTime);
-    if(keys[GLFW_KEY_D])
-        camera.ProcessKeyboard(RIGHT, deltaTime);
+    //GLfloat offsetX = 0.0;//01 * cos(glm::radians(theta));
+    GLfloat offsetY = 0.0005 * sin(glm::radians(theta));
+
+    if((keys[GLFW_KEY_W])||(keys[GLFW_KEY_S])||(keys[GLFW_KEY_A])||(keys[GLFW_KEY_D])){
+
+        // Camera controls
+        if(keys[GLFW_KEY_W])
+            camera.ProcessKeyboard(FORWARD, deltaTime, offsetY);
+        if(keys[GLFW_KEY_S])
+            camera.ProcessKeyboard(BACKWARD, deltaTime, offsetY);
+        if(keys[GLFW_KEY_A])
+            camera.ProcessKeyboard(LEFT, deltaTime, offsetY);
+        if(keys[GLFW_KEY_D])
+            camera.ProcessKeyboard(RIGHT, deltaTime, offsetY);
+
+        if(theta > 360)
+            theta = 0;
+        else
+            theta += 0.8;
+    }
+
+    if((camera.Position.x > 0.5)&&(camera.Position.x < 1.5)&&(camera.Position.z > -1)&&(camera.Position.z < 1)&&(keys[GLFW_KEY_E]))
+        enableKey = false;
+
+    if((camera.Position.x > -7.5)&&(camera.Position.x < -5)&&(camera.Position.z > -5)&&(camera.Position.z < -3)&&(keys[GLFW_KEY_E])&&(!enableKey))
+        enableExitKey = true;
 }
 
 // Is called whenever a key is pressed/released via GLFW
@@ -172,10 +240,13 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
 
+    if((camera.Position.x > -7.5)&&(camera.Position.x < -5)&&(camera.Position.z > -5)&&(camera.Position.z < -3)&&(keys[GLFW_KEY_E])&&(!enableKey))
+        glfwSetWindowShouldClose(window, GL_TRUE);
+
     if(action == GLFW_PRESS)
         keys[key] = true;
     else if(action == GLFW_RELEASE)
-        keys[key] = false;	
+        keys[key] = false;
 }
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
@@ -188,13 +259,13 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
     }
 
     GLfloat xoffset = xpos - lastX;
-    GLfloat yoffset = lastY - ypos; 
-    
+    GLfloat yoffset = lastY - ypos;
+
     lastX = xpos;
     lastY = ypos;
 
     camera.ProcessMouseMovement(xoffset, yoffset);
-}	
+}
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
